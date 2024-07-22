f3d1f08527c337443f203d90233fada1053838b0
Sierru 139 <humanoidgg@gmail.com>
2024-07-22 13:39:28 +0800

    add responsive to navbar and home

--- script.js
+++ script.js
@@ -20,3 +20,23 @@ const swiper = new Swiper('.swiper', {
         clickable: true
     }
 })
+
+const respoNav = document.getElementById('resNav');
+const respoMenu = document.getElementById('resMenu');
+respoNav.addEventListener('click', function() {
+    console.log("Terpencet");
+    respoMenu.classList.remove('nav-close')
+    if (respoMenu.style.display == "none") {
+        respoMenu.style.display = "block"
+    } else {
+        respoMenu.style.display = "none"
+    }
+});
+
+const respoNavClose = document.getElementById('resNavClose')
+respoNavClose.addEventListener('click', function() {
+    respoMenu.classList.add('nav-close')
+    setTimeout(() => {
+        respoMenu.style.display = "none"
+    }, 800);
+})
