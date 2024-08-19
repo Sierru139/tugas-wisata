047a7676c526b184fd6b8927a1a2f7a9815892b4
Sierru139 <145208284+Sierru139@users.noreply.github.com>
2024-08-20 07:04:16 +0800

    adding things

--- script.js
+++ script.js
@@ -72,3 +72,20 @@ function toggleFullScreen() {
         }  
     }  
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const questions = document.querySelectorAll('.ask');
+    questions.forEach(question => {
+        question.addEventListener('click', function() {
+            const answer = this.querySelector('.answer');
+            const arrow = this.querySelector('.arrow-rotate');
+            if (arrow) {
+                arrow.classList.toggle('arrow-rotated');
+            }
+            if (answer) {
+                answer.classList.toggle('show');
+            }
+        });
+    });
+});
+
